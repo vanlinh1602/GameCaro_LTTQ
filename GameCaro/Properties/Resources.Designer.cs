8f38d29a467490050880a6dfb5e8ec31603bd1c2
vanlinh1602 <vanlinh14121@gmail.com>
2021-10-27 10:50:56 +0700

    Update new code

--- GameCaro/Properties/Resources.Designer.cs
+++ GameCaro/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace GameCaro.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap hello {
+            get {
+                object obj = ResourceManager.GetObject("hello", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icon {
             get {
                 object obj = ResourceManager.GetObject("icon", resourceCulture);
